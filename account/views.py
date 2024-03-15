b62c8cb66e014949ddb8d46894464215c5cb37f8
Your Name <your.email@example.com>
2024-03-16 00:02:36 +0600

    login updated

--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # view for registering users
@@ -11,3 +13,16 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+    
+    
+    
+class LogOutAPIView(APIView):
+    def post(self, request, format=None):
+        try:
+            refresh_token = request.data.get('refresh_token')
+            token_obj = RefreshToken(refresh_token)
+            token_obj.blacklist()
+            return Response(status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
